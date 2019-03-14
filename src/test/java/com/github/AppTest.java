c270f2ec0e7c081b717ae706fb91a34527bdc856
abhbirad <abhibiradar1406@gmail.com>
2019-03-15 01:55:45 +0530

    New code added

--- src/test/java/com/github/AppTest.java
+++ src/test/java/com/github/AppTest.java
@@ -1,8 +1,13 @@
-package com.mycompany.app;
+package com.github;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+//Pending comments
+
+//Some comments on the master branch
+//Added some comments on the branch for the egit demo
 
 /**
  * Unit test for simple App.
